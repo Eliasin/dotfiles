8514a55ebcaeb6fdb3bcee7ef6e6ebe8fb608482
Steven Pham <steven.pham.8.2000@gmail.com>
2023-11-18 22:43:42 -0500

    Update

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -133,11 +133,11 @@
        ;;fstar             ; (dependent) types and (monadic) effects and Z3
        ;;gdscript          ; the language you waited for
        ;;(go +lsp)         ; the hipster dialect
-       (haskell +dante)  ; a language that's lazier than I am
+       ;;(haskell +dante)  ; a language that's lazier than I am
        ;;hy                ; readability of scheme w/ speed of python
        ;;idris             ;
        json              ; At least it ain't XML
-       (java +meghanada) ; the poster child for carpal tunnel syndrome
+       ;;(java +meghanada) ; the poster child for carpal tunnel syndrome
        (javascript +lsp)        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
@@ -145,7 +145,7 @@
        ;;lean
        ;;factor
        ;;ledger            ; an accounting system in Emacs
-       lua               ; one-based indices? one-based indices
+       ;;lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
        ;;nix               ; I hereby declare "nix geht mehr!"
@@ -160,7 +160,7 @@
        ;;raku              ; the artist formerly known as perl6
        ;;rest              ; Emacs as a REST client
        ;;rst               ; ReST in peace
-       (ruby +rails +lsp +rbenv)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
+       ;;(ruby +rails +lsp +rbenv)     ; 1.step {|i| p "Ruby is #{i.even? ? 'love' : 'life'}"}
        (rust +lsp)              ; Fe2O3.unwrap().unwrap().unwrap().unwrap()
        ;;scala             ; java, but good
        ;;scheme            ; a fully conniving family of lisps
@@ -173,7 +173,7 @@
        yaml              ; JSON, but readable
 
        :email
-       (mu4e +gmail)
+       ;;(mu4e +gmail)
        ;;notmuch
        ;;(wanderlust +gmail)
 
--- .profile
+++ .profile
@@ -1,10 +1,9 @@
-export EDITOR=/usr/bin/nano
+export EDITOR=/usr/bin/nvim
 export QT_QPA_PLATFORMTHEME="qt5ct"
 export QT_AUTO_SCREEN_SCALE_FACTOR=0
 export GTK2_RC_FILES="$HOME/.gtkrc-2.0"
-. "$HOME/.cargo/env"
 # Add doom to path
-export PATH=$PATH:/home/steven/.emacs.d/bin
+export PATH=$PATH:/home/steven/.config/emacs/bin
 # Add local binaries to path
 export PATH=$PATH:/home/steven/.local/bin
 # Add npm local binaries to path
@@ -18,4 +17,4 @@ export MOZ_ENABLE_WAYLAND=1
 export OBSIDIAN_USE_WAYLAND=1
 
 export SSH_AUTH_SOCK="${XDG_RUNTIME_DIR}/gcr/ssh"
-
+export GTK_THEME=Adwaita:dark 
